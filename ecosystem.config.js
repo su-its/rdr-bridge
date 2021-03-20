51b1afda774bff64cb0b93e2687de1093057f7be
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-03-20 17:27:42 +0900

    stdout、stderrのバッファリングを無効化するために-uオプションを追加

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'rdr-bridge',
     script: 'main.py',
     interpreter: 'python3',
+    interpreter_args: '-u',
     watch: false,
     restart_delay: 1000,
     log_date_format: 'YYYY-MM-DD HH:mm Z',
